7aa8c71c83b9350e9f07ad8958cbb96e40cace50
Thomas <tom.pasqui00@gmail.com>
2022-05-11 18:46:06 +0200

    frontend code base

---
+++ frontend/pages/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+    <van-nav-bar title="Omnia Darts">
+      <template #right>
+        <van-icon name="https://cdn.jsdelivr.net/npm/@vant/assets/icon-demo.png" />Username
+      </template>
+    </van-nav-bar>
+
+    <nuxt-link
+      v-for="locale in availableLocales"
+      :key="locale.code"
+      :to="switchLocalePath(locale.code)">{{ locale.name }}</nuxt-link>
+
+    <h2>{{ $t('hello') }}</h2>
+
+    <van-row justify="space-between">
+      <van-col><van-button type="primary" size="large" to="tournaments">Tornei</van-button></van-col>
+    </van-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+  computed: {
+    availableLocales () {
+      return this.$i18n.locales
+    }
+  }
+}
+</script>
---
+++ frontend/pages/tournaments.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container">
+    <van-nav-bar
+      title="Omnia Darts"
+      left-arrow
+      @click-left="back">
+      <template #right>
+        <van-icon name="https://cdn.jsdelivr.net/npm/@vant/assets/icon-demo.png" />Username
+      </template>
+    </van-nav-bar>
+
+    <h1>Tournaments</h1>
+    <h2>{{ $t('hello') }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "tournaments",
+  methods: {
+    back() { window.history.back() }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
