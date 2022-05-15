4752879f2dab9594190cd0890b2ef9cce2c80d52
Itakello <maxste000@gmail.com>
2022-05-15 14:55:44 +0200

    Backend fixes

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,10 +3,9 @@ import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  app.enableCors();
   /**
    * Swagger configuration
    */
--- backend/src/modules/auth/auth.controller.ts
+++ backend/src/modules/auth/auth.controller.ts
@@ -1,5 +1,27 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Res, UnauthorizedException, UseGuards, InternalServerErrorException, Req, Logger, NotFoundException } from '@nestjs/common';
-import { ApiBearerAuth, ApiCreatedResponse, ApiNotFoundResponse, ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Res,
+  UnauthorizedException,
+  UseGuards,
+  InternalServerErrorException,
+  Req,
+  Logger,
+  NotFoundException,
+} from '@nestjs/common';
+import {
+  ApiBearerAuth,
+  ApiCreatedResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags,
+} from '@nestjs/swagger';
 import { User } from '../../schemas/user.schema';
 import { UsersService } from '../users/users.service';
 import { AuthService } from './auth.service';
@@ -11,33 +33,31 @@ import { ConfigService } from '@nestjs/config';
 @Controller('auth')
 @ApiTags('auth')
 export class AuthController {
-
   constructor(
     private readonly config: ConfigService,
     private readonly authService: AuthService,
-    private readonly userService: UsersService) {}
+    private readonly userService: UsersService,
+  ) {}
 
   @Post()
-  @ApiOperation({ description: "Login a user in" })
-  @ApiOkResponse({ description: "A user logged in successful"})
-  async login(@Body() user : Partial<User>){
-    if(await this.authService.validateUser(user.nickname, user.pwd))
-      return await this.authService.login(user);
-    else
-      throw new UnauthorizedException();
+  @ApiOperation({ description: 'Login a user in' })
+  @ApiOkResponse({ description: 'A user logged in successful' })
+  async login(@Body() user: Partial<User>) {
+    const u = await this.authService.validateUser(user.nickname, user.pwd);
+    if (u) return await this.authService.login(user);
+    else throw new UnauthorizedException();
   }
 
   @Post('register')
-  @ApiOperation({ description: "Register a user" })
-  @ApiCreatedResponse({ description: "User register successful" })
-  @ApiNotFoundResponse({ description: "User not found" })
-  async register(@Body() user : User){
-    
-    const saltOrRounds : number = parseInt(this.config.get('SALTROUNDS'), 10);
+  @ApiOperation({ description: 'Register a user' })
+  @ApiCreatedResponse({ description: 'User register successful' })
+  @ApiNotFoundResponse({ description: 'User not found' })
+  async register(@Body() user: User) {
+    const saltOrRounds: number = parseInt(this.config.get('SALTROUNDS'), 10);
     user.pwd = await bcrypt.hash(user.pwd.toString(), saltOrRounds as number);
     const u = await this.userService.create(user);
 
-    if(u) return await this.authService.login(user);
+    if (u) return await this.authService.login(user);
     else throw new InternalServerErrorException();
   }
 
@@ -48,15 +68,14 @@ export class AuthController {
     return req.user;
   }
 
-  @UseGuards(AuthGuard("google"))
-  @Get("google")
+  @UseGuards(AuthGuard('google'))
+  @Get('google')
   async signInWithGoogle() {}
 
-  @UseGuards(AuthGuard("google"))
-  @ApiOperation({ description: "Sign in with google" })
-  @Get("google/redirect")
+  @UseGuards(AuthGuard('google'))
+  @ApiOperation({ description: 'Sign in with google' })
+  @Get('google/redirect')
   async signInWithGoogleRedirect(@Req() req) {
     return this.authService.signInWithGoogle(req);
   }
-  
 }
