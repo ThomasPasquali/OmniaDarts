f463bb2168a708558b9b434b3befba8f9d9a4687
davidevecchi <davide.vecchi.64@gmail.com>
2022-05-15 16:24:37 +0200

    added profile page UI

---
+++ frontend/pages/profile.vue
@@ -0,0 +1,90 @@
+import { createApp } from 'vue'; import { Form, Field, CellGroup } from 'vant';
+
+<template>
+  <div class="container">
+    <h1>Edit profile</h1>
+    <form @submit.prevent="submit">
+      <input
+        id="photo"
+        type="file"
+      />
+      <br> <br>
+      <ProfileField
+        v-for="field in fields"
+        :label="field.label"
+        :type="field.type"
+        :required="field.required"
+      />
+      <input type="submit" value="Save" />
+    </form>
+  </div>
+</template>
+
+<script>
+import ProfileField from '~/components/ProfileField.vue';
+
+export default {
+  name: "profile",
+  components: {ProfileField},
+  methods: {
+    submit() {
+      // this.$axios
+      //   .$post("/auth", {nickname: this.usr, pwd: this.pwd})
+      //   .then((body) => {
+      //     // console.log(body);
+      //     let {access_token} = body;
+      //     // console.log(access_token);
+      //     this.failedLogin = false;
+      //     localStorage.setItem("auth", access_token);
+      //     this.$axios.setToken(access_token, "Bearer");
+      //     this.$router.push("/");
+      //   })
+      //   .catch((_) => (this.failedLogin = true));
+    },
+  },
+  data() {
+    return {
+      fields: [
+        {label: "Nickname", type: "text", required: true},
+        {label: "First name", type: "text", required: false},
+        {label: "Last name", type: "text", required: false},
+        {label: "Country", type: "country"},
+        {label: "Darts", type: "text", required: false},
+        {label: "Team", type: "text", required: false}
+      ]
+    };
+  },
+}
+</script>
+
+<style>
+input[type=text], select {
+  width: 100%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  display: block;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+}
+
+input[type=submit] {
+  width: 100%;
+  background-color: #04AA6D;
+  color: white;
+  padding: 14px 20px;
+  margin: 8px 0;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+input[type=submit]:hover {
+  background-color: #45a049;
+}
+
+.container {
+  border-radius: 5px;
+  padding: 20px;
+}
+</style>
