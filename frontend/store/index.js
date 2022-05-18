7965279e8a2a4d1546512e0fadfa6bfdef751a1b
Thomas <tom.pasqui00@gmail.com>
2022-05-18 21:21:48 +0200

    finalize merge

--- frontend/store/index.js
+++ frontend/store/index.js
@@ -1,10 +1,27 @@
 export const state = () => ({
-    counter: 0
-  })
-  
-  export const mutations = {
-    increment(state) {
-      state.counter++
-    }
+  sockets: {},
+})
+
+export const actions = {
+  action({ state, commit }, a){
+    //commit('set', a)
   }
-  
+}
+
+export const mutations = {
+  set(state, a) {
+  },
+}
+
+export const getters = {
+  newIo: state => (app, channel) => {
+    //if(!state.sockets[channel])state.sockets[channel] =
+    return app.$nuxtSocket({
+      channel,
+      forceNew: false,
+      persist: true,
+      auth: { token: app.$auth.strategy.token.get() }
+    })
+    //return state.sockets[channel]
+  },
+}
