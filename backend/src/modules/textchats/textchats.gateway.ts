a17e12308d70ae025ab7b954abd2f2a107ec8f0a
Thomas <tom.pasqui00@gmail.com>
2022-05-19 15:22:33 +0200

    Socket IO master server + namespace server

--- backend/src/modules/textchats/textchats.gateway.ts
+++ backend/src/modules/textchats/textchats.gateway.ts
@@ -14,6 +14,7 @@ import { Server } from 'socket.io';
 import { Socket } from "net";
 import { Req } from "@nestjs/common";
 import { SocketIOBodyUnwrapper } from "../../utils/utils";
+import SocketIOServer from "../../utils/socketio.server";
 
 @WebSocketGateway({
     namespace: 'textchats',
@@ -21,41 +22,16 @@ import { SocketIOBodyUnwrapper } from "../../utils/utils";
         origin: '*',
     },
 })
-export class TextchatsGateway implements OnGatewayConnection, OnGatewayDisconnect, OnGatewayInit {
+export class TextchatsGateway extends SocketIOServer {
     @WebSocketServer()
     server: Server;
 
-    clients=[];
-
-    afterInit(server: any): any { }
-
-    handleConnection(client: any) {
-        this.clients.push(client);
-        console.log("New client");
-    }
-
-    handleDisconnect(client) {
-        for (let i = 0; i < this.clients.length; i++) {
-            if (this.clients[i] === client) {
-                this.clients.splice(i, 1);
-                console.log("Client left");
-                break;
-            }
-        }
-        //this.broadcast('disconnect',{});
-    }
-
-    private broadcast(event:string, payload: any) {
-        for (let c of this.clients)
-            c.emit(event, payload);
-    }
-
     @SubscribeMessage('newMessage')
-    newNotification(@MessageBody() body: any): void {
+    async newNotification(@MessageBody() body: any): Promise<void> {
         console.log("New message", body);
         body.data.sender = 'Backend'
+
         setTimeout(_ => this.broadcast('newMessage', { chatID: 'dev', message: body.data }), 2000)
     }
-
 
 }
