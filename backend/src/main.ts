fcecc8bfd1647109416acb0e3ec34c16957cdf43
davide <davide.bergamasco-1@studenti.unitn.it>
2022-05-31 16:19:33 +0200

    fix heroku deploy 10

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,12 +2,17 @@ import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { VersioningType } from '@nestjs/common';
+import { NestFactoryStatic } from '@nestjs/core/nest-factory';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const apiFactory = new NestFactoryStatic();
+  const app = await apiFactory.create(AppModule);
   app.enableCors({
     origin: '*',
   });
+
+  app.setGlobalPrefix('/api/v1/');
 
   /**
    * Swagger configuration
@@ -25,13 +30,14 @@ async function bootstrap() {
     .addTag('tournament-matches')
     .setVersion('1.0')
     .addBearerAuth()
+      .setBasePath('v1')
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
   const configService = app.get<ConfigService>(ConfigService);
-  await app.listen(configService.get('SERVICE_PORT'));
+  await app.listen(configService.get('PORT'));
 }
 
 bootstrap();
