042bc8e5d91d0550949c561fa5d2b0f10f1e2ccc
Itakello <maxste000@gmail.com>
2022-06-01 14:51:29 +0200

    add tournament player

--- backend/src/schemas/match.schema.ts
+++ backend/src/schemas/match.schema.ts
@@ -1,23 +1,22 @@
-import {Prop, raw, Schema, SchemaFactory} from '@nestjs/mongoose';
+import { Prop, raw, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { ApiProperty } from '@nestjs/swagger';
 import mongoose, { Document } from 'mongoose';
+import { TournamentPlayer } from 'src/classes/tournamentPlayer';
+import Lobby from '../classes/lobby';
 import { User } from './user.schema';
-import Lobby from "../classes/lobby";
-import {FirstBest, GamemodeName, SetsLegs} from "../enums/matches";
-import {MatchSettings} from "../interfaces/match";
-import TournamentTypes from "../enums/tournamentTypes";
-import {getEnumDescription} from "../utils/utils";
 
 export type MatchDocument = Match & Document;
 
 @Schema()
 export class Match extends Document {
-
-  @Prop({ default: new Date()})
+  @Prop({ default: new Date() })
   @ApiProperty()
   dateTime: Date;
 
-  @Prop({ type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }], default: [] })
+  @Prop({
+    type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+    default: [],
+  })
   @ApiProperty()
   players: User[];
 
@@ -27,21 +26,28 @@ export class Match extends Document {
   @Prop({ type: Boolean, default: false })
   done: boolean;
 
-  @Prop(raw({
-    name: { type: String/*GamemodeName*/ },
-    settings: { type: Object/*MatchSettings*/ },
-  }))
+  @Prop(
+    raw({
+      name: { type: String /*GamemodeName*/ },
+      settings: { type: Object /*MatchSettings*/ },
+    }),
+  )
   @ApiProperty()
   gamemode: Record<string, any>;
 
-  @Prop(raw({
-    goal: { type: Number },
-    firstBest: { type: String },/*{ enum: () => FirstBest }*/
-    setsLegs: { type: String }, /*{ enum: () => SetsLegs }*/
-  }))
+  @Prop(
+    raw({
+      goal: { type: Number },
+      firstBest: { type: String } /*{ enum: () => FirstBest }*/,
+      setsLegs: { type: String } /*{ enum: () => SetsLegs }*/,
+    }),
+  )
   @ApiProperty()
   winningMode: Record<string, any>;
 
+  @Prop()
+  @ApiProperty()
+  results: TournamentPlayer[];
 }
 
 export const MatchSchema = SchemaFactory.createForClass(Match);
