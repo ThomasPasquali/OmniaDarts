8e879f842a46d97f001f00203cbac3ba1f9b1e19
davidevecchi <davide.vecchi.64@gmail.com>
2022-06-06 10:00:49 +0200

    added user visualization page

---
+++ frontend/components/Profile/ProfileField.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <label for="input">{{ field.label }}</label>
+    <Countries
+      v-if="field.type === 'country'"
+      :field="field"
+      id="input"
+    />
+    <input
+      v-if="field.type === 'text'"
+      id="input"
+      :type="field.type"
+      :required="field.required"
+      v-model="field.value"
+    />
+    <div class="error" v-if="error">Nickname already in use</div>
+  </div>
+</template>
+
+<script>
+import Countries from "~/components/Countries";
+
+export default {
+  name: "ProfileField",
+  components: {Countries},
+  props: ['field', 'attr', 'error']
+}
+</script>
+
+<style scoped>
+.error {
+  color: red;
+  padding-bottom: 1rem;
+}
+</style>
