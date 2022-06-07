5ffccfd5fd58c0af5ac8fec67a503d68cd6c533f
davidevecchi <davide.vecchi.64@gmail.com>
2022-06-07 22:40:52 +0200

    final fixes

--- frontend/pages/stats.vue
+++ frontend/pages/stats.vue
@@ -1,26 +1,27 @@
 <template>
   <div class="container">
 
-    <table class="thead">
-      <tr>
-        <td></td>
-        <td v-for="user in users" :key="user.name">{{ user.name }}</td>
-      </tr>
-    </table>
+    <pre>{{ stats }}</pre>
+<!--    <table class="thead">-->
+<!--      <tr>-->
+<!--        <td></td>-->
+<!--        <td v-for="user in users" :key="user.name">{{ user.name }}</td>-->
+<!--      </tr>-->
+<!--    </table>-->
 
-    <van-collapse v-model="activeNames">
-      <van-collapse-item
-        v-for="group in Object.entries(users[0].stats)"
-        :key="group[0]"
-        :title="group[0]">
-        <table>
-          <tr v-for="stat in Object.keys(group[1])" :key="stat">
-            <td style="color: #0f69ff">{{ stat }}</td>
-            <td style="color: #04AA6D" v-for="user in users" :key="user.name">{{ user.stats[group[0]][stat] }}</td>
-          </tr>
-        </table>
-      </van-collapse-item>
-    </van-collapse>
+<!--    <van-collapse v-model="activeNames">-->
+<!--      <van-collapse-item-->
+<!--        v-for="group in Object.entries(users[0].stats)"-->
+<!--        :key="group[0]"-->
+<!--        :title="group[0]">-->
+<!--        <table>-->
+<!--          <tr v-for="stat in Object.keys(group[1])" :key="stat">-->
+<!--            <td style="color: #0f69ff">{{ stat }}</td>-->
+<!--            <td style="color: #04AA6D" v-for="user in users" :key="user.name">{{ user.stats[group[0]][stat] }}</td>-->
+<!--          </tr>-->
+<!--        </table>-->
+<!--      </van-collapse-item>-->
+<!--    </van-collapse>-->
 
   </div>
 </template>
@@ -34,9 +35,12 @@ export default {
       activeNames: []
     }
   },
+  async mounted() {
+    await this.$store.dispatch("stats/fetchStats");
+  },
   computed: {
-    users() {
-      return this.$store.getters['stats/usersCmp'];
+    stats() {
+      return this.$store.getters['stats/stats'];
     }
   }
 };
